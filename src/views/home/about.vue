6499dff4efe5a2357cadc9b744616a0a831a7a4f
zhaoth <87121452>
2020-05-06 14:53:18 +0800

    去掉多余信息

--- src/views/home/about.vue
+++ src/views/home/about.vue
@@ -5,16 +5,6 @@
       <div class="list">
         <div class="logo"></div>
         <div class="demo-home__title"> VUE H5开发模板</div>
-        <div class="item">项目地址: <a href="https://github.com/sunniejs/vue-h5-template">https://github.com/sunniejs/vue-h5-template</a></div>
-        <div class="item">项目作者: sunnie</div>
-
-        <div class="item"></div>
-        <div class="wechat">
-          <img :src="this.wechat" alt="">
-        </div>
-        <div class="item">关注公众号：回复“加群”即可加 前端仙女群</div>
-        <div class="item">{{userName}} <van-button v-if="userName==''" type="warning" size="small" @click="doDispatch">快点我~</van-button>
-        </div>
       </div>
     </div>
   </div>
@@ -93,7 +83,7 @@ export default {
       .logo {
         width: 120px;
         height: 120px;
-        background: url($cdn+'/weapp/logo.png') center / contain no-repeat;
+
       }
       .wechat {
         width: 200px;
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="app-container">
     <div class="warpper">
-      <h1 class="demo-home__title"><img src="https://imgs.solui.cn/weapp/logo.png" /><span> VUE H5开发模板</span></h1>
+      <h1 class="demo-home__title"><span> VUE H5开发模板</span></h1>
       <h2 class="demo-home__desc">
         A vue h5 template with Vant UI
       </h2>
@@ -16,24 +16,8 @@ export default {
   data() {
     return {
       list: [
-        'Vue-cli4',
-        '配置多环境变量',
-        'VantUI 组件按需加载',
-        'Sass 全局样式',
-        'Webpack 4',
-        'Vuex 状态管理',
-        'Axios 封装及接口管理',
-        'Vue-router',
-        'Webpack 4 vue.config.js 基础配置',
-        '配置 proxy 跨域',
-        '配置 alias 别名',
-        '配置 打包分析',
-        '配置 externals 引入 cdn 资源',
-        '去掉 console.log',
-        'splitChunks 单独打包第三方模块',
-        '添加 IE 兼容',
-        'Eslint+Pettier 统一开发规范'
-
+        '列表demo1',
+        '列表demo2'
       ]
     }
   },
